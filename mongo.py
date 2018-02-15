6c4ea934fef1e99dc4174426b464128b47c14e7a
JFreud <jrm.frdnbrg@gmail.com>
2018-02-14 22:51:53 -0500

    eh prob finished

--- mongo.py
+++ mongo.py
@@ -21,18 +21,21 @@ def zip(zipco):
     return L
 
 def zip_grade(zipco, grade):
-    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades[0].grade" : grade})
+    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades.grade" : grade})
     L = list()
     for restau in boro_cursor:
         L.append(restau)
     return L
 
 def zip_score(zipco, score):
-    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades[0].score" : {$lt: score}})
-    return 0
+    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades.score" : {"$lt": score}})
+    L = list()
+    for restau in boro_cursor:
+        L.append(restau)
+    return L
 
-def random_cuisine_in_your_borough_with_a_score_below_a_specified_threshold(boro, cuisine, score):
-    boro_cursor = restaurants.find({"borough" : boro, "cuisine" : cuisine, "grades[0].score" : {$lt: score}})
+def random_cuisine_in_your_borough_with_a_score_above_a_specified_threshold(boro, cuisine, score):
+    boro_cursor = restaurants.find({"borough" : boro, "cuisine" : cuisine, "grades.score" : {"$gte": score}})
     L = list()
     for restau in boro_cursor:
         L.append(restau)
@@ -40,8 +43,8 @@ def random_cuisine_in_your_borough_with_a_score_below_a_specified_threshold(boro
 
 
 if __name__ == "__main__":
-    print borough("Manhattan")
-    print zip("11262")
-    print zip_grade("11262", "A")
-    print zip_score("11262", "7")
-    print random_cuisine_in_your_borough_with_a_score_below_a_specified_threshold("Manhattan", "American", "7")
+    # print borough("Manhattan")
+    # print zip("11262")
+    # print zip_grade("11209", "A")
+    # print zip_score("11209", 4)
+    # print random_cuisine_in_your_borough_with_a_score_above_a_specified_threshold("Manhattan", "American", 7)
