c16c26a49b227ddddd8d5f8a9ccc992e49ed5e64
JFreud <jrm.frdnbrg@gmail.com>
2018-02-14 22:35:29 -0500

    worked on it

--- mongo.py
+++ mongo.py
@@ -1,13 +1,8 @@
-import pymongo
+import pymongo, random
 
 connection = pymongo.MongoClient("homer.stuy.edu");
 db = connection["test"]
 restaurants = db["restaurants"]
-
-
-
-
-
 
 
 def borough(boro):
@@ -18,18 +13,35 @@ def borough(boro):
     return L
 
 
-def zip():
+def zip(zipco):
+    boro_cursor = restaurants.find({"address.zipcode" : zipco})
+    L = list()
+    for restau in boro_cursor:
+        L.append(restau)
+    return L
+
+def zip_grade(zipco, grade):
+    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades[0].grade" : grade})
+    L = list()
+    for restau in boro_cursor:
+        L.append(restau)
+    return L
+
+def zip_score(zipco, score):
+    boro_cursor = restaurants.find({"address.zipcode" : zipco, "grades[0].score" : {$lt: score}})
     return 0
 
-def zip_grade():
-    return 0
-
-def zip_score():
-    return 0
-
-def smth_clever():
-    return 0
+def random_cuisine_in_your_borough_with_a_score_below_a_specified_threshold(boro, cuisine, score):
+    boro_cursor = restaurants.find({"borough" : boro, "cuisine" : cuisine, "grades[0].score" : {$lt: score}})
+    L = list()
+    for restau in boro_cursor:
+        L.append(restau)
+    return random.choice(L)
 
 
 if __name__ == "__main__":
     print borough("Manhattan")
+    print zip("11262")
+    print zip_grade("11262", "A")
+    print zip_score("11262", "7")
+    print random_cuisine_in_your_borough_with_a_score_below_a_specified_threshold("Manhattan", "American", "7")
